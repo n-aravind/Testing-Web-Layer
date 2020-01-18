1c902d7cee1eb592c9ea0c130e76a8f6f0f0c21b
Aravind Reddy Nareddy <nareddy@Aravinds-MacBook-Pro.local>
2020-01-18 15:24:49 -0700

    initial commit

---
+++ src/test/java/com/example/testingweb/TestingWebApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.testingweb;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
